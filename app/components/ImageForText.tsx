9cae27de01b575752f3d242e5803a17f237527e3
gabjuh <jjjjgabesz@gmail.com>
2023-10-25 16:51:05 +0200

    solve biography image url issue

--- app/components/ImageForText.tsx
+++ app/components/ImageForText.tsx
@@ -1,5 +1,6 @@
 import IImageForText from "@/interfaces/IImageForText";
 import Image from "next/image";
+import convertStringToUrlFriendly from "@/helpers/convertStringToUrlFriendly";
 
 const ImageForText: React.FC<IImageForText> = ({
   fileName,
@@ -7,11 +8,14 @@ const ImageForText: React.FC<IImageForText> = ({
   alt,
   classNameForImg,
 }) => {
+
+  const friendlyFileName = convertStringToUrlFriendly(fileName ?? '');
+
   return (
     <>
       <div className="w-full lg:w-[40%]">
         <Image
-          src={`https://${process.env.NEXT_PUBLIC_BACKEND_API}/img/${fileName}`}
+          src={`https://${process.env.NEXT_PUBLIC_BACKEND_API}/img/${friendlyFileName}`}
           // placeholder="blur"
           width="400"
           height="200"
